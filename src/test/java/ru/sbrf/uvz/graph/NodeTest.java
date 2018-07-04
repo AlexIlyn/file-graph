aa2fb55f54f84ead6c675c2998dd5f4260bb10e5
AlexeyIliin <zaakjin@yandex.ru>
2018-07-04 16:47:31 +0300

    -

--- src/test/java/ru/sbrf/uvz/graph/NodeTest.java
+++ src/test/java/ru/sbrf/uvz/graph/NodeTest.java
@@ -1,57 +1,55 @@
 package ru.sbrf.uvz.graph;
 
+import lombok.var;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 
-class NodeTest {
+public class NodeTest {
 
     Graph graphMock = mock(Graph.class);
 
-//    @Test
-//    public void addDependent_ThrowIllegalAgrgumentException_SameKey() {
-//        var nodeParent = new Node(SubCaseType.DRPA_CUST);
-//        var nodeChild = new Node(SubCaseType.DRPA_CUST);
+    @Test(expected = IllegalArgumentException.class)
+    public void addDependent_ThrowIllegalAgrgumentException_SameKey() {
+        var nodeParent = new Node(SubCaseType.DRPA_CUST);
+        var nodeChild = new Node(SubCaseType.DRPA_CUST);
+        nodeParent.addDependent(nodeChild);
 //        assertThrows(IllegalArgumentException.class,
 //                () -> nodeParent.addDependent(nodeChild));
-//
-//    }
 
-//    @Test
-//    public void addDependent_ThrowIllegalAgrgumentException_DifferentGraph() {
-//        var nodeParent = new Node(SubCaseType.DRPA_CUST);
-//        var nodeChild = new Node(SubCaseType.SAPBO_DOCS);
-//        nodeParent.setGraph(graphMock);
-//        nodeChild.setGraph(mock(Graph.class));
-//        assertThrows(IllegalArgumentException.class,
-//                ()-> nodeParent.addDependent(nodeChild));
-//
-//    }
 
-//    @Test
-//    public void addDependent_ValidEdge() {
-//        var nodeParent = new Node(SubCaseType.DRPA_CUST);
-//        var nodeChild = new Node(SubCaseType.SAPBO_DOCS);
-//        nodeParent.setGraph(graphMock);
-//        nodeChild.setGraph(graphMock);
-//        nodeParent.addDependent(nodeChild);
-//        assertEquals(nodeParent, nodeParent.getNeighbors().get(0).getStart());
-//        assertEquals(nodeChild, nodeParent.getNeighbors().get(0).getEnd());
-//        assertEquals(nodeParent, nodeChild.getNeighbors().get(0).getStart());
-//        assertEquals(nodeChild, nodeChild.getNeighbors().get(0).getEnd());
-//    }
-//
-//    @Test
-//    public void equals_ReturnFalse() {
-//        var nodeOne = new Node(SubCaseType.DRPA_CUST);
-//        var nodeTwo = new Node(SubCaseType.SAPBO_DOCS);
-//        assertFalse(nodeOne.equals(nodeTwo));
-//    }
-//
-//    @Test
-//    public void equals_ReturnTrue() {
-//        var nodeOne = new Node(SubCaseType.DRPA_CUST);
-//        var nodeTwo = new Node(SubCaseType.DRPA_CUST);
-//        assertTrue(nodeOne.equals(nodeTwo));
-//    }
+    }
+
+
+    @Test
+    public void addDependent_ValidEdge() {
+        var nodeParent = new Node(SubCaseType.DRPA_CUST);
+        var nodeChild = new Node(SubCaseType.SAPBO_DOCS);
+        nodeParent.setGraph(graphMock);
+        nodeChild.setGraph(graphMock);
+        nodeParent.addDependent(nodeChild);
+        assertEquals(nodeParent, nodeParent.getNeighbors().get(0).getStart());
+        assertEquals(nodeChild, nodeParent.getNeighbors().get(0).getEnd());
+        assertEquals(nodeParent, nodeChild.getNeighbors().get(0).getStart());
+        assertEquals(nodeChild, nodeChild.getNeighbors().get(0).getEnd());
+    }
+
+    @Test
+    public void equals_ReturnFalse() {
+        var nodeOne = new Node(SubCaseType.DRPA_CUST);
+        var nodeTwo = new Node(SubCaseType.SAPBO_DOCS);
+        assertFalse(nodeOne.equals(nodeTwo));
+    }
+
+    @Test
+    public void equals_ReturnTrue() {
+        var nodeOne = new Node(SubCaseType.DRPA_CUST);
+        var nodeTwo = new Node(SubCaseType.DRPA_CUST);
+        assertTrue(nodeOne.equals(nodeTwo));
+    }
 //
 //    @Test
 //    public void getNeighbors() {
